d51fd035648944368ca10bed5115c0ae2d0dbdce
George Schneeloch <gschneel@mit.edu>
2018-02-02 15:58:50 -0500

    Too long name

--- rapid_response_xblock/logger.py
+++ rapid_response_xblock/logger.py
@@ -8,7 +8,8 @@ from track.backends import BaseBackend
 
 class LoggerBackend(BaseBackend):
     """
-    Record events emitted by blocks. See TRACKING_BACKENDS for the configuration for this logger.
+    Record events emitted by blocks.
+    See TRACKING_BACKENDS for the configuration for this logger.
     For more information about events see:
 
     http://edx.readthedocs.io/projects/devdata/en/stable/internal_data_formats/tracking_logs.html
