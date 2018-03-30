5c249a6de3b71c61d4dd19515268678bf2b482ae
Gavin Sidebottom <gsidebo@mit.edu>
2018-03-30 14:13:48 -0400

    Updated Django dependency range

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     author="MITx",
     zip_safe=False,
     install_requires=[
-        'django>=1.8,<=1.11',
+        'django>=1.8,<2.0',
         'XBlock',
         'xblock-utils',
         'edx-opaque-keys'
